60b3f4d441371bc9aebc0a1f33875078ae6366d1
Lorik Mehmeti <lorikkmehmeti@gmail.com>
2024-02-11 02:59:16 +0100

    change import

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -1,7 +1,6 @@
+import { cn } from "@/lib/utils.ts";
 import * as TabsPrimitive from "@radix-ui/react-tabs";
 import * as React from "react";
-
-import { cn } from "../../../../feature-flags/src/lib/utils.ts";
 
 const Tabs = TabsPrimitive.Root;
 
